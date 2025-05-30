825e9a4ef25f0014c640e13073dcce1465426b06
Dekiuv <diego1roque2@gmail.com>
2025-05-30 12:28:18 +0200

    update 30/5

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ window.addEventListener("scroll", () => {
 
 const typedText = document.querySelector(".typed-text");
 
-const words = ["Especialista IA", "Analista de datos", "Administrador de sistemas", "Programador junior", "Gestor de redes"];
+const words = ["Especialista Inteligencia Artificial", "Analista de Datos", "Administrador de Sistemas", "Programador", "Gestor de Redes"];
 let wordIndex = 0;
 let charIndex = 0;
 let isDeleting = false;
